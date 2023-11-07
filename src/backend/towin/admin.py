c2fff1ac0a69d50124a78923dd8c48ea161749d6
Андрей Варачев <Dartanyun@yandex.com>
2023-11-07 22:27:18 +0500

    All new models added to admin panel
    Also created custom adminModel class

--- src/backend/towin/admin.py
+++ src/backend/towin/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from core.models import EmptyFieldModel
+from .models import Order, Feedback, TowTruck, Tariff, Price
 
-# Register your models here.
+
+class OdrderAdmin(EmptyFieldModel):
+    list_display = ('client', 'address_from', 'address_to', 'created',
+                    'price', 'addition', 'delay', 'tow_truck')
+
+
+class FeedbackAdmin(EmptyFieldModel):
+    list_display = ('score', 'comment', 'order')
+
+
+class TowTruckAdmin(EmptyFieldModel):
+    list_display = ('is_active', 'driver')
+
+
+class TariffAdmin(EmptyFieldModel):
+    list_display = ('name', 'description', 'price')
+
+
+class PriceAdmin(EmptyFieldModel):
+    list_display = ('tariff', 'car_type', 'wheel_lock', 'towin')
+
+
+admin.site.register(Order, OdrderAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(TowTruck, TowTruckAdmin)
+admin.site.register(Tariff, TariffAdmin)
+admin.site.register(Price, PriceAdmin)
--- src/backend/user/admin.py
+++ src/backend/user/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
+from core.models import EmptyFieldModel
 
 User = get_user_model()
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(EmptyFieldModel):
     list_display = ('username', 'email', 'first_name', 'last_name', 'role')
     search_fields = ('username', 'email')
     list_filter = ('username', 'email')
     list_editable = ('role',)
-    empty_value_display = '-пусто-'
 
     def save_model(self, request, obj, form, change):
         """Хэширует пароль и сохраняет его в базе данных"""

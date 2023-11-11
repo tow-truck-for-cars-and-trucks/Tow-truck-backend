5fe1e7ac3abceb20f45e232c4eac452a2e5336b7
Михаил Алексенцев <legolas961@gmail.com>
2023-11-11 14:10:38 +0300

    Откат

--- src/backend/towin/admin.py
+++ src/backend/towin/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from core.models import MinValidatedInlineMixIn
 from core.models import EmptyFieldModel
-from .models import Order, Feedback, TowTruck, Tariff, PriceOrder
+from .models import Order, Feedback, TowTruck, Tariff, PriceOrder, CarType
 
 
 class OrderriceTabularInline(admin.TabularInline, MinValidatedInlineMixIn):
@@ -11,26 +11,36 @@ class OrderriceTabularInline(admin.TabularInline, MinValidatedInlineMixIn):
 
 
 class OdrderAdmin(EmptyFieldModel):
-    list_display = ('client', 'address_from', 'address_to', 'created',
-                    'addition', 'delay', 'tow_truck')
-    inlines = [
-        OrderriceTabularInline
-    ]
+    list_display = (
+        "client",
+        "address_from",
+        "address_to",
+        "created",
+        "addition",
+        "delay",
+        "tow_truck",
+    )
+    inlines = [OrderriceTabularInline]
 
 
 class FeedbackAdmin(EmptyFieldModel):
-    list_display = ('score', 'comment', 'order')
+    list_display = ("score", "comment", "order")
 
 
 class TowTruckAdmin(EmptyFieldModel):
-    list_display = ('is_active', 'driver')
+    list_display = ("is_active", "driver")
 
 
 class TariffAdmin(EmptyFieldModel):
-    list_display = ('name', 'description', 'price')
+    list_display = ("name", "description", "price")
+
+
+class CarTypeAdmin(EmptyFieldModel):
+    list_display = ("car_type", "car_type_price")
 
 
 admin.site.register(Order, OdrderAdmin)
 admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(TowTruck, TowTruckAdmin)
 admin.site.register(Tariff, TariffAdmin)
+admin.site.register(CarType, CarTypeAdmin)
--- src/backend/towin/models.py
+++ src/backend/towin/models.py
@@ -10,18 +10,19 @@ class TowTruck(models.Model):
     """
     Модель эвакуатора.
     """
+
     is_active = models.BooleanField(
-        verbose_name='Статус эвакуатора.',
+        verbose_name="Статус эвакуатора.",
     )
     driver = models.CharField(
-        verbose_name='Водитель',
-        help_text='Укажите водителя',
+        verbose_name="Водитель",
+        help_text="Укажите водителя",
         max_length=255,
     )
 
     class Meta:
-        verbose_name = 'Эвакуатор'
-        verbose_name_plural = 'Эвакуаторы'
+        verbose_name = "Эвакуатор"
+        verbose_name_plural = "Эвакуаторы"
 
     def __str__(self) -> str:
         return self.driver
@@ -31,65 +32,69 @@ class Tariff(models.Model):
     """
     Модель тарифа.
     """
+
     name = models.CharField(
-        verbose_name='Название тарифа',
+        verbose_name="Название тарифа",
         max_length=50,
-        choices=TariffChoices.choices
+        choices=TariffChoices.choices,
     )
     description = models.CharField(
-        verbose_name='Описание тарифа',
-        max_length=255
+        verbose_name="Описание тарифа", max_length=255
     )
-    price = models.IntegerField(
-        verbose_name='Цена тарифа',
-        validators=[MinValueValidator(1)]
+    price = models.PositiveSmallIntegerField(
+        verbose_name="Цена тарифа", validators=[MinValueValidator(1)]
     )
 
     class Meta:
-        verbose_name = 'Тариф'
-        verbose_name_plural = 'Тарифы'
+        verbose_name = "Тариф"
+        verbose_name_plural = "Тарифы"
 
     def __str__(self) -> str:
         return self.name
+
+
+class CarType(models.Model):
+    car_type = models.CharField(
+        "Тип машины", choices=VenchiceTypeChoices.choices
+    )
+    car_type_price = models.PositiveSmallIntegerField(
+        verbose_name="Цена за тип авто",
+        validators=[MinValueValidator(1)],
+    )
+
+    class Meta:
+        verbose_name = "Тип авто"
+        verbose_name_plural = "Типы авто"
+        default_related_name = "car_type"
+
+    def __str__(self):
+        return self.car_type
 
 
 class Order(models.Model):
     """
     Модель заказа.
     """
+
     client = models.ForeignKey(
-        User,
-        verbose_name='Пользователь',
-        on_delete=models.CASCADE
+        User, verbose_name="Пользователь", on_delete=models.CASCADE
     )
     address_from = models.CharField(
-        verbose_name='Адрес подачи',
-        max_length=200
+        verbose_name="Адрес подачи", max_length=200
     )
     address_to = models.CharField(
-        verbose_name='Адрес прибытия',
-        max_length=200
+        verbose_name="Адрес прибытия", max_length=200
     )
-    addition = models.CharField(
-        verbose_name='Комментарий',
-        max_length=300
-    )
-    delay = models.BooleanField(
-        verbose_name='Задержка'
-    )
+    addition = models.CharField(verbose_name="Комментарий", max_length=300)
+    delay = models.BooleanField(verbose_name="Задержка")
     tow_truck = models.ForeignKey(
-        TowTruck,
-        on_delete=models.CASCADE,
-        verbose_name='Эвакуатор'
+        TowTruck, on_delete=models.CASCADE, verbose_name="Эвакуатор"
     )
-    created = models.DateTimeField(
-        'Дата заказа',
-        auto_now_add=True
-    )
+    created = models.DateTimeField("Дата заказа", auto_now_add=True)
 
     class Meta:
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
 
     def __str__(self) -> str:
         return str(self.pk)
@@ -102,41 +107,38 @@ class PriceOrder(models.Model):
     В итоге можно будет запрашивать
     все заказы пользователя.
     """
+
     tariff = models.ForeignKey(
         Tariff,
         on_delete=models.CASCADE,
-        verbose_name='Тариф',
+        verbose_name="Тариф",
         max_length=30,
     )
-    car_type = models.CharField(
-        verbose_name='Тип машины',
+    car_type = models.ForeignKey(
+        CarType,
+        on_delete=models.CASCADE,
+        verbose_name="Тип авто",
         max_length=30,
-        choices=VenchiceTypeChoices.choices
     )
-    wheel_lock = models.IntegerField(
-        verbose_name='Заблокированные колеса',
+    wheel_lock = models.PositiveSmallIntegerField(
+        verbose_name="Заблокированные колеса",
         validators=[MinValueValidator(0), MaxValueValidator(4)],
-        default=0
+        default=0,
     )
-    towin = models.BooleanField(
-        verbose_name='Кюветные работы'
-    )
+    towin = models.BooleanField(verbose_name="Кюветные работы")
     order = models.ForeignKey(
         Order,
         on_delete=models.CASCADE,
-        verbose_name='Заказ',
+        verbose_name="Заказ",
     )
 
     class Meta:
-        ordering = ('order',)
-        verbose_name = 'Заказы и Цены'
-        verbose_name_plural = 'Заказы и цены'
+        ordering = ("order",)
+        verbose_name = "Заказы и Цены"
+        verbose_name_plural = "Заказы и цены"
 
         constraints = [
-            models.UniqueConstraint(
-                fields=['order'],
-                name='unique_order'
-            )
+            models.UniqueConstraint(fields=["order"], name="unique_order")
         ]
 
     def __str__(self) -> str:
@@ -147,24 +149,22 @@ class Feedback(models.Model):
     """
     Модель оценки заказа.
     """
-    score = models.IntegerField(
-        verbose_name='Оценка',
-        validators=[MinValueValidator(0), MaxValueValidator(5)]
+
+    score = models.PositiveSmallIntegerField(
+        verbose_name="Оценка",
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
     )
-    comment = models.CharField(
-        verbose_name='Комментарий',
-        max_length=400
-    )
+    comment = models.CharField(verbose_name="Комментарий", max_length=400)
     order = models.ForeignKey(
         Order,
         on_delete=models.CASCADE,
-        verbose_name='Заказ',
-        related_name='score'
+        verbose_name="Заказ",
+        related_name="score",
     )
 
     class Meta:
-        verbose_name = 'Отзыв'
-        verbose_name_plural = 'Отзывы'
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
 
     def __str__(self) -> str:
         return self.order

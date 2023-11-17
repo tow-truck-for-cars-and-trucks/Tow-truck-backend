a52119606027e4799bf0fa8c1075919f7bee5203
Mikhail <119411618+Aleksentcev@users.noreply.github.com>
2023-11-17 15:00:31 +0300

    Update models.py

--- src/backend/towin/models.py
+++ src/backend/towin/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
 from core.validators import plate_validator
 from core.choices import TariffChoices, VenchiceTypeChoices
 

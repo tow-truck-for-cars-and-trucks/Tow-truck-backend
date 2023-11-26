031a8b4484024d3e91277baeb46063cc76dca131
Mikhail <119411618+Aleksentcev@users.noreply.github.com>
2023-11-26 13:23:34 +0300

    Update settings.py

--- src/backend/tow_truck/settings.py
+++ src/backend/tow_truck/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False") in ("True", "true", "t", 1)
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split()
 

c56cd387d945637743aac34c9e16ad5312de00d5
Mikhail <119411618+Aleksentcev@users.noreply.github.com>
2023-12-26 13:55:07 +0300

    Update models.py

--- src/backend/towin/models.py
+++ src/backend/towin/models.py
@@ -63,7 +63,7 @@ class Tariff(models.Model):
         verbose_name="Цена тарифа", validators=[MinValueValidator(1)]
     )
     delivery_time = models.TimeField("Время подачи", null=True)
-    info = models.TextField(
+    info = models.CharField(
         verbose_name="Расширенная информация о тарифе", 
         max_length=255,
         default=''
